9a193bfff712daa98ee159a663df5b4e6bef9880
CYBERWOLF333 <161057375+CYBERWOLF333@users.noreply.github.com>
2025-03-27 16:27:03 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'XUL_V2 ', // Your name
+  ownerNumber: '9052292612', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
